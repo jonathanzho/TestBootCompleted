b2e68f7e2a340225530cb4aa822fa8db4ebaa8d4
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2019-05-12 22:03:07 -0700

    make Action name a static String.

--- app/src/main/java/com/example/jonathan/testbootcompleted/MainActivity.java
+++ app/src/main/java/com/example/jonathan/testbootcompleted/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
   private static final String TAG = " TBC " + "MainActivity";
 
   private TextView mTextView;
-  private String mActionName;
+  private static String sActionName;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
     // Get the Intent that started this activity and extract the string
     Intent intent = getIntent();
-    mActionName = intent.getStringExtra("actionName");
-    Log.v(TAG, "onCreate: mActionName=[" + mActionName + "]");
+
+    if(sActionName == null) {
+      sActionName = intent.getStringExtra("actionName");
+      Log.v(TAG, "onCreate: sActionName=[" + sActionName + "]");
+    }
 
     // Capture the layout's TextView
     mTextView = findViewById(R.id.tvActionName);
@@ -34,10 +37,10 @@ public class MainActivity extends AppCompatActivity {
 
     super.onResume();
 
-    Log.v(TAG, "onResume: mActionName=[" + mActionName + "]");
+    Log.v(TAG, "onResume: sActionName=[" + sActionName + "]");
 
     // Set the string as its text
-    mTextView.setText("[" + mActionName + "]");
+    mTextView.setText("[" + sActionName + "]");
   }
 
   @Override
